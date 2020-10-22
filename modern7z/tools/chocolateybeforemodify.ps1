8bcb132472b0ce96f3b5bfbe27e04ae448349346
Илья Зверев <izverev@logrocon.com>
2020-10-22 16:58:27 +0300

    init

---
+++ modern7z/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$installed = Get-Content -Path $env:ChocolateyPackageFolder\tools\installed.json -Raw | ConvertFrom-Json
+
+$spliter = "path to executable:"
+$7zLocation = "$(Split-Path -parent ((7z --shimgen-noop | Select-String $spliter) -split $spliter | ForEach-Object Trim)[1])"
+$installLocation = "$(Join-Path $7zLocation "Codecs")"
+
+Write-Host "Remove libraries" -ForegroundColor Blue
+ForEach($file in $installed) {
+  Remove-Item "$(Join-Path $installLocation $file)" -Force
+}
+Write-Host "Remove completed" -ForegroundColor Blue
---
+++ modern7z/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$meta = Get-Content -Path $env:ChocolateyPackageFolder\tools\packageArgs.json -Raw
+$packageArgs = @{}
+(ConvertFrom-Json $meta).psobject.properties | ForEach-Object { $packageArgs[$_.Name] = $_.Value }
+
+$filename = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true) {
+       Split-Path $packageArgs["url64bit"] -Leaf }
+else { Split-Path $packageArgs["url"] -Leaf }
+
+$packageArgs["packageName"] = "$($env:ChocolateyPackageName)"
+$packageArgs["fileFullPath"] = "$(Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) $filename)"
+
+$archiveLocation = Get-ChocolateyWebFile @packageArgs
+$extractLocation = "$(Join-Path (Split-Path -parent $archiveLocation) "Codecs")"
+
+$spliter = "path to executable:"
+$7zLocation = "$(Split-Path -parent ((7z --shimgen-noop | Select-String $spliter) -split $spliter | ForEach-Object Trim)[1])"
+$installLocation = "$(Join-Path $7zLocation "Codecs")"
+
+Write-Host "Install libraries" -ForegroundColor Blue
+
+New-Item -ItemType directory -Path $installLocation -Force
+Expand-Archive -LiteralPath $archiveLocation -DestinationPath $extractLocation
+if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true) {
+       $extractLocationArch = Join-Path $extractLocation '64'
+} else {
+       $extractLocationArch = Join-Path $extractLocation '32'
+}
+
+Get-ChildItem -Recurse $extractLocationArch -Name -File | ConvertTo-Json | Out-File $env:ChocolateyPackageFolder\tools\installed.json
+Copy-Item "$(Join-Path $extractLocationArch '/*')" "$($installLocation)" -Recurse -Force
+
+Write-Host "Install completed" -ForegroundColor Blue
